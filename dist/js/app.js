a03c513fcfdef57547ad8c0f2ffbe17cbcb1bf0c
shatalovdm <innoshatalov@gmail.com>
2017-04-14 22:23:52 -0500

    Created Yelp bot to talk to Yelp API

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,40 @@
-$(function () {
-	'use strict';
+$(document).ready(function() {
+	var config;
 
-	// kick things off by creating the `App`
-	new app.AppView();
+	$.getJSON('config.json', function(json) {
+	    console.log(json); // this will show the info it in firebug console
+	    config = json;
+	});
+
+	$.ajax({
+	    url: 'https://api.yelp.com/v3/businesses/search',
+	    method: 'GET',
+	    data: 'term=food&location=Chicago',
+	    cache: true,
+  		dataType: 'jsonp',
+  		jsonp : false,
+    	jsonpCallback: 'jsonCallback',
+    	beforeSend: function(xhr) { xhr.setRequestHeader("Authorization", 
+    		"Bearer " + btoa("api:" + "SofPnaGSPTbeHmaz1jL7ffk9o11W5M9TD1E2vxPhQRTZSL3O3L12zeL6SuKlES2jM5thVxuqJh-crHBJnEYCstRw0XvO4L8EgsaYFFWe8e9_9WWYqO4lcO_3FArxWHYx")); },
+	    //headers: {"Authorization":"Bearer SofPnaGSPTbeHmaz1jL7ffk9o11W5M9TD1E2vxPhQRTZSL3O3L12zeL6SuKlES2jM5thVxuqJh-crHBJnEYCstRw0XvO4L8EgsaYFFWe8e9_9WWYqO4lcO_3FArxWHYx"},
+	    success: function(data) {
+	    	console.log(data);
+	    }
+	})
+	.fail(function(jqXHR, textStatus, errorThrown) {
+	                                    console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+	                        }
+	                );
+
+
+
 });
+
+
+
+
+
+
+
+
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,43 @@
-$(function () {
-	'use strict';
+$(document).ready(function() {
+	var config;
 
-	// kick things off by creating the `App`
-	new app.AppView();
+	$.getJSON('config.json', function(json) {
+	    console.log(json); // this will show the info it in firebug console
+	    config = json;
+	});
+
+	$.ajax({
+	    url: 'https://api.yelp.com/v3/businesses/search',
+	    method: 'GET',
+	    data: 'term=food&location=Chicago',
+	    dataType: 'jsonp',
+	    headers: {"Authorization":"Bearer SofPnaGSPTbeHmaz1jL7ffk9o11W5M9TD1E2vxPhQRTZSL3O3L12zeL6SuKlES2jM5thVxuqJh-crHBJnEYCstRw0XvO4L8EgsaYFFWe8e9_9WWYqO4lcO_3FArxWHYx"},
+	    xhrFields: {
+	    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+	    // This can be used to set the 'withCredentials' property.
+	    // Set the value to 'true' if you'd like to pass cookies to the server.
+	    // If this is enabled, your server must respond with the header
+	    // 'Access-Control-Allow-Credentials: true'.
+	    withCredentials: false
+	  	},
+	    success: function(data) {
+	    	console.log(data);
+	    }
+	})
+	.fail(function(jqXHR, textStatus, errorThrown) {
+	                                    console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+	                        }
+	                );
+
+
+
 });
+
+
+
+
+
+
+
+
+

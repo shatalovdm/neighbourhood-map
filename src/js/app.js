08d25264e522eb89393b2846013dd991b2b436a9
shatalovdm <innoshatalov@gmail.com>
2017-04-16 18:40:55 -0500

    Added AJAX request to OpenTable API

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,45 @@
+
+
 $(document).ready(function() {
-	var config;
 
-	$.getJSON('config.json', function(json) {
-	    console.log(json); // this will show the info it in firebug console
-	    config = json;
-	});
+	'use strict';
 
-	$.ajax({
-	    url: 'https://api.yelp.com/v3/businesses/search',
-	    method: 'GET',
-	    data: 'term=food&location=Chicago',
-	    dataType: 'jsonp',
-	    headers: {"Authorization":"Bearer SofPnaGSPTbeHmaz1jL7ffk9o11W5M9TD1E2vxPhQRTZSL3O3L12zeL6SuKlES2jM5thVxuqJh-crHBJnEYCstRw0XvO4L8EgsaYFFWe8e9_9WWYqO4lcO_3FArxWHYx"},
-	    xhrFields: {
-	    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-	    // This can be used to set the 'withCredentials' property.
-	    // Set the value to 'true' if you'd like to pass cookies to the server.
-	    // If this is enabled, your server must respond with the header
-	    // 'Access-Control-Allow-Credentials: true'.
-	    withCredentials: false
-	  	},
-	    success: function(data) {
-	    	console.log(data);
-	    }
-	})
-	.fail(function(jqXHR, textStatus, errorThrown) {
-	                                    console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
-	                        }
-	                );
+	var Restaurant = function(name, address, lat, lng, zip, phone, price, image, link) {
+		this.name = name;
+		this.address = address;
+		this.lat = lat;
+		this.lng = lng;
+		this.zip = zip;
+		this.phone = phone;
+		this.price = price;
+		this.image = image;
+		this.link = link;
+	}
 
+	var RestaurantsModel = function() {
+		var self = this;
 
+		self.restaurants = ko.observableArray();
 
+	    $.ajax({
+		    url: 'http://opentable.herokuapp.com/api/restaurants',
+		    method: 'GET',
+		    data: 'zip=60613',
+	  		dataType: 'jsonp',
+		 
+		    success: function(data) {
+
+		    	for (var i = 0; i < data.restaurants.length; i++) {
+		    		var restaurant = data.restaurants[i];
+		    		self.restaurants.push(new Restaurant(restaurant.name, restaurant.address, restaurant.lat, 
+        			restaurant.lng, restaurant.postal_code, restaurant.phone,
+        			restaurant.price, restaurant.image_url, restaurant.reserve_url))
+		    	}
+		    }
+		})
+	}
+
+	ko.applyBindings(new RestaurantsModel());
 });
 
 

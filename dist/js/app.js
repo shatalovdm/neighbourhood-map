fb1abae735db16d051916755da197210bd339a04
shatalovdm <innoshatalov@gmail.com>
2017-04-19 12:13:02 -0500

    Added functionality to highlight markers on hover

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,7 +12,8 @@ $(document).ready(function() {
 	ko.applyBindings(restaurantsViewModel);
 });
 
-var Restaurant = function(name, address, lat, lng, phone, price, image, link) {
+var Restaurant = function(id, name, address, lat, lng, phone, price, image, link) {
+	this.id = id;
 	this.name = name;
 	this.address = address;
 	this.lat = lat;
@@ -26,6 +27,7 @@ var Restaurant = function(name, address, lat, lng, phone, price, image, link) {
 var RestaurantsViewModel = function() {
 	var self = this;
 
+	self.restaurantId = ko.observable('');
 	self.restaurants = ko.observableArray();
 	self.filterName = ko.observable('');
 	self.filterAddress = ko.observable('');
@@ -40,6 +42,24 @@ var RestaurantsViewModel = function() {
     		$("caption").html("Results");
     		getMapData();
     	}
+	}
+
+	self.enableMarker = function(restaurant) {
+		for (var i = 0; i < markers.length; i++) {
+			if (markers[i].id === restaurant.id) {
+				highlightMarker(markers[i]);
+				break;
+			}
+		}
+	}
+
+	self.disableMarker = function(restaurant) {
+		for (var i = 0; i < markers.length; i++) {
+			if (markers[i].id === restaurant.id) {
+				revertMarker(markers[i]);
+				break;
+			}
+		}
 	}
 
   	self.filterRestaurants = ko.computed(function() {
@@ -98,7 +118,7 @@ function getOpenTableData() {
 		    			default:
 		    				price = '';
 		    		}
-		    		restaurantsViewModel.restaurants.push(new Restaurant(restaurant.name, address, restaurant.lat, 
+		    		restaurantsViewModel.restaurants.push(new Restaurant(restaurant.id, restaurant.name, address, restaurant.lat, 
 	    			restaurant.lng, restaurant.phone, price, restaurant.image_url, restaurant.reserve_url));
 		    	}
 	    		// Populate the map with markers
@@ -115,6 +135,9 @@ function getOpenTableData() {
 
 // Get map from Google Maps API centered by 'zip' code
 function getMapData() {
+	// Map style provided by StipeP on snazzymaps.com
+	var styles = [{"featureType":"all","elementType":"geometry.fill","stylers":[{"weight":"2.00"}]},{"featureType":"all","elementType":"geometry.stroke","stylers":[{"color":"#9c9c9c"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#eeeeee"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#7b7b7b"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c8d7d4"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#070707"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]}]
+
 	geocoder = new google.maps.Geocoder();
 	var zip = restaurantsViewModel.zipSearch();
 	zip = !zip ? '60601': zip;
@@ -123,6 +146,7 @@ function getMapData() {
 		if (status == google.maps.GeocoderStatus.OK) {
 			map = new google.maps.Map(document.getElementById('map'), {
 				center: results[0].geometry.location,
+				styles: styles,
 				scrollwheel: false,
 				zoom: 15
 			});
@@ -140,29 +164,62 @@ function populateMap (restaurants) {
 	var location;
 	var restaurant;
 
+	// If the are any markers already, remove them from the map
 	if (markers) { removeMarkers(); }
+
 
 	for (var i = 0; i < restaurants.length; i++) {
 		restaurant = restaurants[i];
 		location = {lat: restaurant.lat, lng: restaurant.lng};
+		// Creare a marker
 		var marker = new google.maps.Marker({
 		    position: location,
 		    map: map,
 		    title: restaurant.name,
-		    id: i
+		    id: restaurant.id
         });
 
         markers.push(marker);
 
         marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-         });
+            populateInfoWindow(this, restaurant, largeInfowindow);
+        });
+
+
+
+        // Expend the map coverage to show all the markers
         bounds.extend(markers[i].position);
-        // marker.click(function() {
-        // 	populateInfoWindow(this, largeInfoWindow);
-        // });
 	}
-	
+	if (restaurants.length > 0) {
+		map.panToBounds(bounds);
+	}
+}
+
+function highlightMarker(marker) {
+	var image = {
+	  url: 'img/marker.png',
+	  origin: new google.maps.Point(0, 0),
+	  scaledSize: new google.maps.Size(50, 50)
+	};
+	marker.setIcon(image);
+}
+
+function revertMarker(marker) {
+	marker.setIcon();
+}
+
+function populateInfoWindow(marker, restaurant, infowindow) {
+// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>' + '<div>' + restaurant.address + '</div>' + 
+			'<div>' + '<a class="btn btn-primary" href="restaurant.link" role="button">Reserve a table</a>' + '</div>');
+		infowindow.open(map, marker);
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+        });
+	}
 }
 
 // Remove all markers from the map
@@ -170,6 +227,7 @@ function removeMarkers(){
     for(var i=0; i<markers.length; i++){
         markers[i].setMap(null);
     }
+    markers.length = 0;
 }
 
 var s = document.createElement("script");
